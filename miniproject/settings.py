c7902941f15ea6408140d74e42b85f370346f3a0
PetsINDIA <deninjose0@gmail.com>
2024-09-07 22:01:52 +0530

    commit

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where collectstatic will place files
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'miniproject/static'),
+    os.path.join(BASE_DIR, 'miniproject/static'),  # This includes your static files during development
 ]
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
