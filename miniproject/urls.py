0a8f900ba4f7250225ccac62dac30e748b146684
PetsINDIA <deninjose0@gmail.com>
2024-09-30 15:52:25 +0530

    seeds app correction

--- miniproject/urls.py
+++ miniproject/urls.py
@@ -25,7 +25,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('',include('pets.urls')),
+    path('',include('seeds.urls')),
     path('accounts/', include("django.contrib.auth.urls")),
    
 
--- seeds/apps.py
+++ seeds/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PetsConfig(AppConfig):
+class SeedsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pets'
+    name = 'seeds'
